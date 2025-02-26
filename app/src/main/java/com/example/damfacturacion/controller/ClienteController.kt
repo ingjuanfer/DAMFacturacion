68be526038908ff9202565310636970eee0b94ea
ingjuanfer <ingjuanfer@gmail.com>
2025-02-26 22:29:21 +0100

    Delete Cliente

--- app/src/main/java/com/example/damfacturacion/controller/ClienteController.kt
+++ app/src/main/java/com/example/damfacturacion/controller/ClienteController.kt
@@ -1,7 +1,9 @@
 package com.example.damfacturacion.controller
 
+import com.example.damfacturacion.controller.ProductoController.ErrorResponse
 import com.example.damfacturacion.interfaces.ClienteService
 import com.example.damfacturacion.model.Cliente
+import com.example.damfacturacion.model.Producto
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import retrofit2.Retrofit
@@ -43,6 +45,55 @@ class ClienteController {
             }
         })
     }
+
+    fun obtenerCliente(token: String, nIF: String, callback: (Cliente?, String) -> Unit) {
+        val call = clienteService.getClientePorNIF("Bearer $token", nIF)
+
+        call.enqueue(object : Callback<List<Cliente>> {
+            override fun onResponse(call: Call<List<Cliente>>, response: Response<List<Cliente>>) {
+                if (response.isSuccessful && !response.body().isNullOrEmpty()) {
+                    callback(response.body()!![0], "Cliente encontrado")
+                } else {
+                    callback(null, "Cliente no encontrado")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Cliente>>, t: Throwable) {
+                callback(null, "Error de conexión: ${t.message}")
+            }
+        })
+    }
+
+    fun eliminarCliente(token: String, nIF: String, callback: (Boolean , String ) -> Unit) {
+        val call = clienteService.eliminarCliente("Bearer $token", nIF)
+
+        call.enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.isSuccessful) {
+                    callback(true, "Cliente eliminado correctamente")
+                } else {
+                    val errorMessage = parseError(response)
+                    callback(false, errorMessage)
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                callback(false, "Error de conexión: ${t.message}")
+            }
+        })
+    }
+
+    // Método para parsear errores
+    private fun parseError(response: Response<*>): String {
+        val errorBody = response.errorBody()?.string()
+        return try {
+            val errorJson = Gson().fromJson(errorBody, com.example.damfacturacion.controller.ClienteController.ErrorResponse::class.java)
+            errorJson.mensaje ?: "Error desconocido"
+        } catch (e: Exception) {
+            "Error desconocido (${response.code()})"
+        }
+    }
+
 
     // Clase para parsear el error JSON
     data class ErrorResponse(
